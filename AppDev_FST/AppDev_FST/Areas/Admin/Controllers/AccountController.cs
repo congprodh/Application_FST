b353143083d3feee7f676a6950dc53ad1a76330c
congprodh <congprodh@gmail.com>
2021-10-15 05:04:19 +0700

    Create Admin area, Trainee area

---
+++ AppDev_FST/AppDev_FST/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace AppDev_FST.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin/Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppDev_FST/AppDev_FST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppDev_FST.Startup))]
+namespace AppDev_FST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
